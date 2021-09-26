fa4e14ebd3deb1766722a84066e836e2ffb8038a
Victor Vera Frazao <viver101@hhu.de>
2021-09-26 20:38:23 +0200

    Fixed bound calculations

--- src/modify_model.jl
+++ src/modify_model.jl
@@ -27,29 +27,31 @@ end
     function adjust_model(database, variation_tres =.5, dGr_dict =Dict())
 Reduces model to usable reactions. It excludes exchange reactions, those where no Gibbs free energy could be calculated or with too large standard deviation, depending on the given coefficient of variation threshold (`variation_tres`). 
 """
-function adjust_model(database, variation_tres = 0.5)
+function adjust_model(database, variation_tres = 0.9)
     dGr_dict, dGr_bounds = collect_dGr_bounds(database)
 
     for rxn in reactions(database)
-        if startswith(rxn, "EX_") #excluding exchanges
+        if !reaction_mass_balanced(database, rxn) #excluding exchanges
             database = remove_reaction(database, rxn)
-        elseif typeof(dGr_dict[rxn]) == String #excluding reactions without
-            database = remove_reaction(database, rxn)
-        else
-            try
-                if dGr_dict[rxn].val.err / dGr_dict[rxn].val.val > variation_tres
-                    database = remove_reaction(database, rxn)
-                end
-            catch
-                database = remove_reaction(database, rxn)
-            end
         end
     end
-
     mod_bounds = OrderedDict()
     for (k, v) in dGr_bounds
         if k in reactions(database)
             mod_bounds[k] = v
+        end
+    end
+    for rxn in keys(mod_bounds)
+        if typeof(dGr_dict[rxn]) == String #excluding reactions without calculated Gibbs energies
+            mod_bounds[rxn] = (0.0, 0.0)
+        else
+            try
+                if dGr_dict[rxn].val.err / dGr_dict[rxn].val.val > variation_tres || abs(dGr_dict[rxn].val.val) == Inf
+                    mod_bounds[rxn] = (0.0, 0.0)
+                end
+            catch
+                mod_bounds[rxn] = (0.0, 0.0)
+            end
         end
     end
 
