d958872868f4cd57f316219d7ef24338e854db9c
Victor Vera Frazao <viver101@hhu.de>
2021-09-26 20:36:19 +0200

    Added exports for optimisation functions

--- src/OptStoic.jl
+++ src/OptStoic.jl
@@ -6,10 +6,13 @@ using Unitful, Measurements
 using ProgressMeter
 using Suppressor
 using JuMP
+using OrderedCollections
 
+include("thermodynamics.jl")
+include("modify_model.jl")
 include("build_model.jl")
 include("Analysis.jl")
 
-export build_OptStoic_model, build_MinFlux_model
+export build_OptStoic_model, build_MinFlux_model, optimize_OptStoic, MinFlux
 
 end
