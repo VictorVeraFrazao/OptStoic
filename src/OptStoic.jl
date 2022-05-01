74b3e27e5d40785d2991a37568fffdf0edfd9c23
Victor Vera Frazao <viver101@hhu.de>
2022-05-01 12:10:47 +0200

    Added preprocessing.jl

--- src/OptStoic.jl
+++ src/OptStoic.jl
@@ -8,13 +8,16 @@ using JuMP
 using COBREXA
 using OrderedCollections
 using DocStringExtensions
+using LinearAlgebra
+using SparseArrays
 
 include("thermodynamics.jl")
+include("preprocessing.jl")
 include("modify_model.jl")
 include("build_model.jl")
 include("Analysis.jl")
 
-# export everything that isn't prefixed with _ (inspired by JuMP.jl and COBREXA.jl, thanks!)
+# export everything that isn't prefixed with _ (inspired by JuMP.jl and COBREXA.jl)
 for sym in names(@__MODULE__, all = true)
     if sym in [Symbol(@__MODULE__), :eval, :include] || startswith(string(sym), ['_', '#'])
         continue
